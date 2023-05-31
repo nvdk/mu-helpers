5ed9cab307e89f2922c74264f43be88c98098929
Niels Vandekeybus <progster@gmail.com>
2023-05-31 15:25:51 +0200

    initial extraction of code from mu-javascript-template
    includes a few fixes and minimal conversion to typescript

---
+++ README.md
@@ -0,0 +1,17 @@
+# mu-helpers
+
+javascript package providing all the things you can import in the [javascript-template](https://github.com/mu-semtech/mu-javascript-template) of [semantic.works](https://semantic.works).
+
+
+## Development
+To use this locally you'll need to build the typescript and link into your project:
+
+```bash
+git clone <this-repo>
+cd <this-repo-dir>
+npm install
+npm run build
+npm link
+cd <your-code-repo>
+npm link mu-helpers
+```
---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import httpContext from 'express-http-context';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+var app = express();
+
+var port =  parseInt(process.env.PORT) || 80;
+var hostname = process.env.HOST || '0.0.0.0';
+var bodySizeLimit = process.env.MAX_BODY_SIZE || '100kb';
+
+// parse JSONAPI content type
+app.use(bodyParser.json({
+  type: function(req: express.Request) { return /^application\/vnd\.api\+json/.test(req.get('content-type')); },
+  limit: bodySizeLimit
+}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set JSONAPI content type
+app.use('/', function(req, res: express.Response, next) {
+  res.type('application/vnd.api+json');
+  next();
+});
+
+app.use(httpContext.middleware);
+
+app.use(function(req, res, next) {
+  httpContext.set('request', req);
+  httpContext.set('response', res);
+  next();
+});
+
+const errorHandler = function(err, req, res: express.Response, next) {
+  res.status(err.status || 400);
+  res.json({
+    errors: [ {title: err.message} ]
+  });
+};
+
+// start server
+app.listen( port, hostname, function() {
+  console.log(`Starting server on ${hostname}:${port} in ${app.get('env')} mode`);
+});
+
+export default app;
+
+export {
+  app,
+  errorHandler
+}
